c2cf933da8a1ffa1404cb54518a1b40021114bd3
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-04-03 23:04:12 -0500

    Moved display table to own file. Adding Manager viewing functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ const chalk = require("chalk");
 require("dotenv").config();
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const display = require("./displayProducts.js");
 
 // Import MySQL database credentials
 const keys = require("./keys.js");
@@ -33,61 +34,17 @@ function mainMenu() {
         }
     ]).then(function(res) {
         if(res.menu === "SHOP") {
-            displayProducts();
+            getAllProducts();
         } else {
             connection.end();
         }
-    })
+    });
 }
 
-// Show all the products
-function displayProducts() {
+function getAllProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if(err) throw err;
-
-        // Add column titles as a row
-        const colKeys = Object.keys(res[0]);
-        res.unshift({
-            item_id: "ID",
-            product_name: "Product Name",
-            department_name: "Department",
-            price: "Price ($)",
-            stock_quantity: "Stock Quantity"
-        });
-
-        // Determine the width of each column
-        const columnWidths = colKeys.map(col => res.reduce((a, b) => a[col].toString().length > b[col].toString().length ? a : b)[col].toString().length);
-        const totalWidth = columnWidths.reduce((a, b) => a + b, 0) + 16;
-
-        // Top border of table
-        console.log("\n" + "-".repeat(totalWidth));
-
-        // Print each item
-        res.forEach(function(item, index) {
-            if (index === 0) {
-                color = "yellow"
-            } else if(index % 2 === 0) {
-                color = "cyan"
-            } else {
-                color = "white"
-            }
-
-            console.log(chalk[color](
-                chalk.white("| ") + item.item_id + " ".repeat(columnWidths[0] - item.item_id.toString().length) + chalk.white(" | ")
-                + item.product_name + " ".repeat(columnWidths[1] - item.product_name.toString().length) + chalk.white(" | ")
-                + item.department_name + " ".repeat(columnWidths[2] - item.department_name.toString().length) + chalk.white(" | ")
-                + item.price + " ".repeat(columnWidths[3] - item.price.toString().length) + chalk.white(" | ")
-                + item.stock_quantity + " ".repeat(columnWidths[4] - item.stock_quantity.toString().length) + chalk.white(" |")
-            ));
-
-            if (index === 0) {
-                console.log("-".repeat(totalWidth));
-            }
-        });
-
-        // Bottom border of table
-        console.log("-".repeat(totalWidth) + "\n");
-
+        display.displayProducts(res);
         selectProduct(res.length - 1);
     });
 }
@@ -135,8 +92,9 @@ function purchaseProduct(itemId, purchaseQuantity) {
                 ],
                 function(err, resUpdate) {
                     if(err) throw err;
+                    const total = purchaseQuantity * item.price;
                     console.log(chalk.green("\nYou purchased " + purchaseQuantity + " of " + item.product_name));
-                    console.log(chalk.green("Your order total is $" + purchaseQuantity * item.price));
+                    console.log(chalk.green("Your order total is $" + total.toFixed(2)));
                     mainMenu();
                 });
         } else {
---
+++ bamazonManager.js
@@ -0,0 +1,67 @@
+// Require package statements
+const chalk = require("chalk");
+require("dotenv").config();
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const display = require("./displayProducts.js");
+
+// Import MySQL database credentials
+const keys = require("./keys.js");
+
+// Create the MySQL database connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: keys.mySqlDb.user,
+    password: keys.mySqlDb.pw,
+    database: "bamazon_db"
+});
+
+// Connect to the DB
+connection.connect(function(err) {
+    if(err) throw err;
+    mainMenu();
+});
+
+function mainMenu() {
+    console.log();
+    inquirer.prompt([
+        {
+            name: "menu",
+            message: "What would you like to do?",
+            type: "list",
+            choices: ["VIEW PRODUCTS FOR SALE", "VIEW LOW INVENTORY", "ADD TO INVENTORY", "ADD NEW PRODUCT", "EXIT"]
+        }
+    ]).then(function(res) {
+        switch(res.menu) {
+            case "VIEW PRODUCTS FOR SALE":
+                getAllProducts();
+            break;
+            case "VIEW LOW INVENTORY":
+                getLowInventory();
+            break;
+            case "ADD TO INVENTORY":
+            break;
+            case "ADD NEW PRODUCT":
+            default:
+                connection.end();
+            break;
+        }
+    });
+}
+
+function getAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if(err) throw err;
+        display.displayProducts(res);
+        mainMenu();
+    });
+}
+
+function getLowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function(err, res) {
+        if(err) throw err;
+        display.displayProducts(res);
+        mainMenu();
+    })
+}
