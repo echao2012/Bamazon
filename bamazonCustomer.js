8eadfe5a94c7f6495e018d48c1f9be6255e690f7
echao2012 <47439716+echao2012@users.noreply.github.com>
2019-04-03 22:03:21 -0500

    Implemented purchasing products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 // Require package statements
+const chalk = require("chalk");
 require("dotenv").config();
 const inquirer = require("inquirer");
 const mysql = require("mysql");
@@ -21,9 +22,22 @@ connection.connect(function(err) {
     mainMenu();
 });
 
-// Display the main menu
 function mainMenu() {
-    displayProducts();
+    console.log();
+    inquirer.prompt([
+        {
+            name: "menu",
+            message: "What would you like to do?",
+            type: "list",
+            choices: ["SHOP", "EXIT"]
+        }
+    ]).then(function(res) {
+        if(res.menu === "SHOP") {
+            displayProducts();
+        } else {
+            connection.end();
+        }
+    })
 }
 
 // Show all the products
@@ -50,13 +64,21 @@ function displayProducts() {
 
         // Print each item
         res.forEach(function(item, index) {
-            console.log(
-                "| " + item.item_id + " ".repeat(columnWidths[0] - item.item_id.toString().length) + " | "
-                + item.product_name + " ".repeat(columnWidths[1] - item.product_name.toString().length) + " | "
-                + item.department_name + " ".repeat(columnWidths[2] - item.department_name.toString().length) + " | "
-                + item.price + " ".repeat(columnWidths[3] - item.price.toString().length) + " | "
-                + item.stock_quantity + " ".repeat(columnWidths[4] - item.stock_quantity.toString().length) + " |"
-            )
+            if (index === 0) {
+                color = "yellow"
+            } else if(index % 2 === 0) {
+                color = "cyan"
+            } else {
+                color = "white"
+            }
+
+            console.log(chalk[color](
+                chalk.white("| ") + item.item_id + " ".repeat(columnWidths[0] - item.item_id.toString().length) + chalk.white(" | ")
+                + item.product_name + " ".repeat(columnWidths[1] - item.product_name.toString().length) + chalk.white(" | ")
+                + item.department_name + " ".repeat(columnWidths[2] - item.department_name.toString().length) + chalk.white(" | ")
+                + item.price + " ".repeat(columnWidths[3] - item.price.toString().length) + chalk.white(" | ")
+                + item.stock_quantity + " ".repeat(columnWidths[4] - item.stock_quantity.toString().length) + chalk.white(" |")
+            ));
 
             if (index === 0) {
                 console.log("-".repeat(totalWidth));
@@ -65,7 +87,63 @@ function displayProducts() {
 
         // Bottom border of table
         console.log("-".repeat(totalWidth) + "\n");
+
+        selectProduct(res.length - 1);
     });
+}
+
+function selectProduct(numProducts) {
+    // Ask the user for a product ID and amount they want to purchase
+    inquirer.prompt([
+        {
+            name: "id",
+            message: "What is the ID of the item you would like to purchase?",
+            validate: function(value) {
+                return !isNaN(value) && value > 0 && value <= numProducts;
+            }
+        },
+        {
+            name: "quantity",
+            message: "How many would you like to purchase?",
+            validate: function(value) {
+                return !isNaN(value) && value > 0;
+            }
+        }
+    ]).then(function(res) {
+        purchaseProduct(res.id, res.quantity);
+    })
+}
+
+function purchaseProduct(itemId, purchaseQuantity) {
+    // Get the amount in stock
+    connection.query("SELECT * FROM products WHERE item_id = ?", itemId, function(err, resSelect) {
+        if(err) throw err;
+        
+        const item = resSelect[0];
+
+        // Check that there is enough in stock
+        if(item.stock_quantity >= purchaseQuantity) {
+            // Remove the amount purchased from the amount in stock
+            connection.query("UPDATE products SET ? WHERE ?",
+                [
+                    {
+                        stock_quantity: item.stock_quantity - purchaseQuantity
+                    },
+                    {
+                        item_id: itemId
+                    }
+                ],
+                function(err, resUpdate) {
+                    if(err) throw err;
+                    console.log(chalk.green("\nYou purchased " + purchaseQuantity + " of " + item.product_name));
+                    console.log(chalk.green("Your order total is $" + purchaseQuantity * item.price));
+                    mainMenu();
+                });
+        } else {
+            console.log(chalk.red("\nInsufficient stock. Your order was canceled."));
+            mainMenu();
+        }
+    })
 }
 
 // Return a variable amount of characters in a string
